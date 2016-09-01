bc81534ec9ad8b343b59c392a7eeffd3193ca2c8
Matyas Kone <bikesvatera@gmail.com>
2016-09-01 20:27:42 +0200

    Initial commit

---
+++ src/Servlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Matyas on 8/29/2016.
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("mi a fasz shover???");
+
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Matyas
+  Date: 8/29/2016
+  Time: 11:31 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Login Page</title>
+  </head>
+  <body>
+
+  <form>
+
+    First name:<br>
+    <input type="text" name="firstname"><br>
+    Last name:<br>
+    <input type="text" name="lastname"><br>
+    <input type="submit" value="Submit">
+  </form>
+
+  </body>
+</html>
