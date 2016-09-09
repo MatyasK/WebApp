6087554c66477aeae59efe597c8c34d9e74aed93
Matyas Kone <bikesvatera@gmail.com>
2016-09-09 08:53:08 +0200

    Latest Improvments

--- src/OverViewServlet.java
+++ src/OverViewServlet.java
@@ -1,3 +1,5 @@
+import users.User;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -35,6 +37,7 @@ public class OverViewServlet extends HttpServlet {
         }
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
+        ArrayList<User> users = dataModel.getUsers();
 
         out.println("<html>");
         out.println("<head>");
@@ -43,6 +46,18 @@ public class OverViewServlet extends HttpServlet {
         out.println("<body bgcolor=\"white\">");
         out.println("Number of visits: " + numberOfvisits);
         out.println("<br>");
+        out.println("<table border=\"1\">");
+        out.println("<tr>");
+        out.println("<th>Username</th>");
+        out.println("</tr");
+
+
+        for (User user : users) {
+                out.println("<tr>");
+                out.println("<td>" + user.getUserName() + "</td>");
+                out.println("</tr>");
+        }
+        out.println("</table>");
         out.println("<a href=\"LogoutServlet\">Logout</a>");
         out.println("</body>");
         out.println("</html>");
